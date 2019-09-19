04d8f87f9186b0e4e315852d61ad67f03cea0c97
“e <15257988874@163.com>
2019-09-19 09:39:55 +0800

    Site updated: 2019-09-19 09:39:52

--- js/src/funnyTitle.js
+++ js/src/funnyTitle.js
@@ -2,12 +2,12 @@
     var titleTime;
     document.addEventListener('visibilitychange', function () {
         if (document.hidden) {
-            $('[rel="icon"]').attr('href', "/img/trhx2.png");
-            document.title = '阿哦，崩溃了~！';
+            // $('[rel="icon"]').attr('href', "/img/trhx2.png");
+            document.title = '页面崩溃了~！';
             clearTimeout(titleTime);
         }
         else {
-            $('[rel="icon"]').attr('href', "/img/trhx2.png");
+            // $('[rel="icon"]').attr('href', "/img/trhx2.png");
             document.title = '恢复啦~！' + OriginTitle;
             titleTime = setTimeout(function () {
                 document.title = OriginTitle;
